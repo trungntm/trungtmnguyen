387403fb331f79656298853a5adf93e86e3f3320
Nguyễn Tạ Minh Trung <trungntm.97@gmail.com>
2026-07-17 16:28:42 +0700

    feat: dynamic redirect (#37)

--- apps/web/features/redirects/index.ts
+++
@@ -1,5 +0,0 @@
-import { blogRedirects } from './blog-redirects';
-import { seriesRedirects } from './series-redirects';
-import { tagRedirects } from './tag-redirects';
-
-export const permanentRedirects = [...blogRedirects, ...tagRedirects, ...seriesRedirects];
